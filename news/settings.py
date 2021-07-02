90f3f8c6ae2cb056c82f2e6f8f842fa36798c4f2
farahzuot <farahz997@eng.hu.edu.jo>
2021-07-03 01:54:53 +0300

    authentication added

--- news/settings.py
+++ news/settings.py
@@ -132,5 +132,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSIONS_CLASSES' : [
          'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.BasicAuthentication',
+    ],
 }
--- politics/views.py
+++ politics/views.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import PoliticSerializer
 from .models import Politic
@@ -10,10 +11,10 @@ from .permissions import PermissionAdminStaff
 class newsListCreateView(generics.ListAPIView,generics.CreateAPIView):
     queryset = Politic.objects.all()
     serializer_class = PoliticSerializer
-    permission_classes = (PermissionAdminStaff,)
+    permission_classes = (PermissionAdminStaff,IsAuthenticated)
 
 
 class newsDetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Politic.objects.all()
     serializer_class = PoliticSerializer
-    permission_classes = (PermissionAdminStaff,)
+    permission_classes = (PermissionAdminStaff,IsAuthenticated)
